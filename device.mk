1b9944b3cccb1ac18d15e670af0f39889a2155bc
chandra <chandra.chaganti@hotmail.com>
2019-12-20 12:09:30 +0530

    guacamole: Evolution-X Bringup

--- device.mk
+++ device.mk
@@ -14,11 +14,7 @@ $(call inherit-product, vendor/oneplus/guacamole/guacamole-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
+    $(LOCAL_PATH)/overlay
 
 # Camera
 PRODUCT_PACKAGES += \
