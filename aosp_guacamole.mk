420e81172ce7d8d00cf266807dceb8eb9863e42e
chandra <chandra.chaganti@hotmail.com>
2020-01-04 15:04:26 +0530

    guacamole: Ev-X Official tag

--- aosp_guacamole.mk
+++ aosp_guacamole.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/oneplus/guacamole/device.mk)
 # Inherit some common Evolution-X stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+# Official
+CUSTOM_BUILD_TYPE := OFFICIAL
+
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1440
 
